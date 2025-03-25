39527227824b972c3e271fb0e6cba50c00ba451e
BhavSA <BhavanaaR@saglobal.com>
2025-03-25 15:56:36 +0530

    Added the Test file

---
+++ UnitTest1.cs
@@ -0,0 +1,22 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DemoSA
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Navigate()
+        {
+
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Manage().Window.Maximize();
+
+        }
+        //Login part 
+
+    }
+}
