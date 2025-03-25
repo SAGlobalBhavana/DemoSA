103e60e2ef30a6505d66138c6101f5e8895cb6d8
Bhavana <BhavanaaR@saglobal.com>
2025-03-25 16:47:07 +0530

    Update UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -13,10 +13,10 @@ namespace DemoSA
 
             driver.Navigate().GoToUrl("https://www.google.com");
             driver.Manage().Window.Maximize();
-            
+            driver.FindElement(By.XPath("//input[@name=\"email\"]")).SendKeys("BhavanaaR@saglobal.com");
+            driver.FindElement(By.XPath("//input[@name=\"pass\"]")).SendKeys("****");
+            driver.FindElement(By.XPath("//input[@name=\"Submit\"]")).Click();
             driver.Close();
         }
-       
-
     }
 }
