d3611f9c711af32a3c2e89a66ebee33f0540088a
Bhavana <69754483+Bhav-tech@users.noreply.github.com>
2025-03-26 10:25:14 +0530

    Bhav Update  UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -9,6 +9,7 @@ namespace DemoSA
         [TestMethod]
         public void Navigate()
         {
+            // Initialising the driver
             IWebDriver driver = new ChromeDriver();
 
             driver.Navigate().GoToUrl("https://www.google.com");
@@ -16,6 +17,8 @@ namespace DemoSA
             driver.FindElement(By.XPath("//input[@name=\"email\"]")).SendKeys("BhavanaaR@saglobal.com");
             driver.FindElement(By.XPath("//input[@name=\"pass\"]")).SendKeys("****");
             driver.FindElement(By.XPath("//input[@name=\"Submit\"]")).Click();
+            
+            // Close the driver
             driver.Close();
         }
     }
